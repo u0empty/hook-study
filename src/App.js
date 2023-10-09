52aa4fa60efbcac19eb3e4c478ff60a4d6190086
u0empty <yubeen00@naver.com>
2023-10-09 18:02:50 +0900

    'useCallback-study-1'

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
-  const [isKorea, setIsKorea] = useState(true);
+  const [toggle, setToggle] = useState(true);
 
-  const location = useMemo(() => {
-    return {
-      country: isKorea ? "한국" : "외국",
-    };
-  }, [isKorea]);
+  const someFunction = useCallback(() => {
+    console.log(`someFunc: number: ${number}`);
+    return;
+  }, [number]);
 
   useEffect(() => {
-    console.log("useEffect 호출");
-  }, [location]);
+    console.log(`someFunction 이 변경되었습니다.`);
+  }, [someFunction]);
 
   return (
     <div>
-      <h2>몇끼나드셔</h2>
       <input
         type="number"
         value={number}
         onChange={(e) => setNumber(e.target.value)}
       />
-      <hr />
-      <h2>어디셔</h2>
-      <p>나라: {location.country}</p>
-      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
+      <br />
+      <button onClick={someFunction}>Call someFunc</button>
     </div>
   );
 }
