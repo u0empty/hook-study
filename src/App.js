a19ec7fd13faa83aeeb9d9af5a94715365ef99cf
u0empty <yubeen00@naver.com>
2023-10-15 16:07:13 +0900

    ✨ useReducer study - 1
    - gitmessage 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React, { useCallback, useState } from "react";
-import Box from "./Box";
+import React, { useState, useReducer } from "react";
+
+// reducer - state 업데이트 역할 (은행)
+// dispatch - state 업데이트를 위한 요구
+// action - 요구 내용
+
+const ACTION_TYPES = {
+  deposit: "deposit",
+  withdraw: "withdraw",
+};
+
+const reducer = (state, action) => {
+  console.log("reducer가 일을 합니다!", state, action);
+  switch (action.type) {
+    case ACTION_TYPES.deposit:
+      return state + action.payload;
+    case ACTION_TYPES.withdraw:
+      return state - action.payload;
+    default: // 타입이 이상한 경우 일을 하지 않음으로써 실수 방지!
+      return state;
+  }
+};
 
 function App() {
-  const [size, setSize] = useState(100);
-  const [isDark, setIsDark] = useState(false);
-
-  const createBoxStyle = useCallback(() => {
-    return {
-      backgroundColor: "green",
-      width: `${size}px`,
-      height: `${size}px`,
-    };
-  }, [size]);
+  const [number, setNumber] = useState(0);
+  const [money, dispatch] = useReducer(reducer, 0);
 
   return (
-    <div style={{ backgroundColor: isDark ? "black" : "white" }}>
+    <div>
+      <h2>useReducer 은행에 오신 것을 환영합니다</h2>
+      <p>잔고: {money}원</p>
       <input
         type="number"
-        value={size}
-        onChange={(e) => setSize(e.target.value)}
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+        step="1000"
       />
-      <button onClick={(e) => setIsDark(!isDark)}>Change Theme</button>
-      <Box createBoxStyle={createBoxStyle} />
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.deposit, payload: number });
+        }}
+      >
+        예금
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.withdraw, payload: number });
+        }}
+      >
+        출금
+      </button>
     </div>
   );
 }
